6e3c1a927278b6aa4742451500ce13e315a8d2dd
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2020-05-19 22:04:34 +0200

    ui: make activity fullscreen

--- app/src/main/java/org/mrlem/sample/jmeandroid/MainActivity.kt
+++ app/src/main/java/org/mrlem/sample/jmeandroid/MainActivity.kt
@@ -1,13 +1,34 @@
 package org.mrlem.sample.jmeandroid
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * Main activity that loads the JME fragment.
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setupFullscreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        if (hasFocus) {
+            setupFullscreen()
+        }
+    }
+
+}
+
+fun AppCompatActivity.setupFullscreen() {
+    val uiVisibility = window.decorView.systemUiVisibility
+    window.decorView.systemUiVisibility = uiVisibility or
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 }
